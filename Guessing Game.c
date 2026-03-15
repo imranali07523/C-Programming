393dc89a87c6fb8f8bc0586d30e4b704eebdd898
Imran Ali <imranali07523@gmail.com>
2026-03-15 17:39:23 +0530

    Add files via upload

---
+++ Guessing Game.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(){
+    int random,guess;
+    int counter=0;
+    srand(time(NULL));
+    random=rand()%100+1;
+    printf("Welcome to the world of Guessing Number\n");
+    do{
+        printf("\nGuess a number(1-100):");
+        scanf("%d",&guess);
+        counter++;
+        if(guess>random){
+            printf("Guess a smaller number\n");
+        }
+        else if(guess<random){
+            printf("Guess a larger number\n");
+        }
+        else{
+            printf("You have Successfully Guessed the number in %d attempts\n",counter);
+        }
+        }while(guess!=random);
+    printf("Bye Bye\nThanks For Playing\n");
+    printf("Developed By : Imran Ali\n");
+    }
